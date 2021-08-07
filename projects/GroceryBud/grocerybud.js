2fe2b7bfd6ff3e4041f20d8aee0bcb9282ae27c2
Henrique Oliveira <henrique.olive29@gmail.com>
2021-08-07 20:42:42 +0100

    3 exercise

---
+++ projects/GroceryBud/grocerybud.js
@@ -0,0 +1,95 @@
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('.submit-btn');
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clear = document.querySelector('.clear-btn');
+
+//edit option
+
+let editElement;
+let editflag = false;
+let editID="";
+
+//event listeners
+//submit form
+form.addEventListener('submit', addItem);
+//clear items
+clear.addEventListener('click', clearItems)
+
+
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value!=="" && editflag===false){
+       const element = document.createElement('article');
+       //add class
+       element.classList.add('grocery-item');
+       //add id
+       const attr = document.createAttribute('data-id');
+       attr.value = id;
+       element.setAttributeNode(attr);
+       element.innerHTML = `<p class="title">${value}</p>
+       <div class="btn-container">
+           <button type="button" class="edit-btn">
+               <i class="fas fa-edit"></i>
+           </button>
+           <button type="button" class="delete-btn">
+               <i class="fas fa-trash"></i>
+           </button>
+       </div>`;
+       //append child
+       list.appendChild(element);
+       //display alert
+       displayAlert('item added to the list', 'success');
+       //show container
+       container.classList.add('show-container');
+       //add to local storage
+       addToLocalStorage(id,value);
+       //set back to default
+       setBackToDefault();
+    }else if(value!=="" && editflag===true){
+        console.log("editing");
+    }else{
+        displayAlert("please enter value", 'danger')
+    }
+}
+
+
+//clear items
+function clearItems(){
+    const items = document.querySelectorAll('.grocery-item');
+    if(items.length>0){
+        items.forEach(function(item){
+            list.removeChild(item);
+
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert('ampty list', 'danger');
+}
+
+//display alert
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    //remove alert
+    setTimeout(function(){
+        alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+    },1000);
+}
+
+//set back to default
+function setBackToDefault(){
+    grocery.value='';
+    editflag = false;
+    editID = '';
+    submitBtn.textContent = 'submit';
+}
+function addToLocalStorage(id, value){
+    console.log("added to local storage");
+}
